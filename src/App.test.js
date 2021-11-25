b5d1731e34f505afeba418a1e1ee8c37642393e6
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-11-25 20:20:43 -0300

    Atualizar - App.test - Refatora o arquivo de testes do App para renderizar as novas alterações e garantir a qualidade do código

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Renders the App', () => {
+  it('should renders title', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Challenge B8One/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+})
