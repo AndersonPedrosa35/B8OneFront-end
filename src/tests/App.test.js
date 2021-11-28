546a014a026cd4931d520117f9192ed3562aa003
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-11-28 20:56:37 -0300

    Atualizar - ListProducts - Refatorando lógica do botão

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,11 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-describe('Renders the App', () => {
+describe('Renders title in App', () => {
   it('should renders title', () => {
     render(<App />);
     const linkElement = screen.getByText(/Challenge B8One/i);
     expect(linkElement).toBeInTheDocument();
   });
+});
 
+describe('Renders the cards from request', () => {
+  beforeEach(() => {
+    
+  })
+  it('should renders the title of item', () => {
+    render(<App />);
+    const 
+  })
 })
